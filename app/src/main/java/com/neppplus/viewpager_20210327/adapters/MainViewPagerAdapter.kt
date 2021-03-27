564a8171e0fd6de5b2636e0a370e4d55d03b677e
홍준호 <0615wnsgh@gmail.com>
2021-03-27 14:16:31 +0900

    when 조건문 입력

--- app/src/main/java/com/neppplus/viewpager_20210327/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20210327/adapters/MainViewPagerAdapter.kt
@@ -9,18 +9,25 @@ import com.neppplus.viewpager_20210327.Fragments.Page3Fragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+//        리턴을 하는데 -> position 값에 따라 다른 결과가 리턴되도록 하는 코틀린 문법 예시
+        return when(position) {
+            0 -> "인사"
+            1 -> "자기소개"
+            else -> "하고싶은말"
+        }
+    }
+
+
 //    필수 오버라이딩 2개를 구현 => Alt + Enther
 
     override fun getItem(position: Int): Fragment {
 //        position의 값에 따라 다른 Fragment 리턴.
-        if (position == 0) {
-            return Page1Fragment()
-        }
-        else if (position == 1) {
-            return Page2Fragment()
-        }
-        else {
-            return Page3Fragment()
+
+        return when(position) {
+            0 -> Page1Fragment()
+            1 -> Page2Fragment()
+            else -> Page3Fragment()
         }
     }
 
